4b3cb2701a9872177a6eca408ab52e768fff2ec4
Dersen <georgebryamramosjaldin@gmail.com>
2025-12-11 17:08:37 -0400

    Subida inicial

---
+++ app.py
@@ -0,0 +1,5 @@
+
+import os
+class calculadora:
+    def sum(self,a:int,b:int) -> int:
+        return 0#a + b
---
+++ test.py
@@ -0,0 +1,3 @@
+from app import calculadora
+def test_sums_2_numeros():
+    assert calculadora().sum(2,2) ==4
